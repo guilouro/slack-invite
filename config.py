d60b229d7d4a1a11bd6cb0cdd61c2497d18235d0
Guilherme Louro <guilherme-louro@hotmail.com>
2015-07-31 15:24:13 -0300

    social meta tags

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
-SLACK_GROUP = 'YOUR_GROUP_NAME'
-SLACK_TOKEN = 'TOKEN_GROUP'
+# coding: utf-8
+
+SLACK_GROUP = 'pythonbrasil'
+SLACK_TOKEN = 'xoxp-8064483091-8064250496-8099154530-e482b8'
 SLACK_URL = 'https://%s.slack.com' % SLACK_GROUP
+
+META_TITLE = u'Participe do comunidade PythonBrasil no Slack'
+META_DESCRIPTION = u"""
+    Comunidade Python Brasil no Slack.
+    Slack é um app de comunicação com suporte a canais, conversas,
+    integração com seriços externos além de ser viciante."""
