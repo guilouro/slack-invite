aaa99e297837ca2e73f6d351add715ac5afe6715
Guilherme Louro <guilherme-louro@hotmail.com>
2015-07-31 15:36:51 -0300

    config remove infos

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
 # coding: utf-8
 
-SLACK_GROUP = 'pythonbrasil'
-SLACK_TOKEN = 'xoxp-8064483091-8064250496-8099154530-e482b8'
+SLACK_GROUP = 'YOUR_GROUP_NAME'
+SLACK_TOKEN = 'TOKEN_GROUP'
 SLACK_URL = 'https://%s.slack.com' % SLACK_GROUP
 
-META_TITLE = u'Participe do comunidade PythonBrasil no Slack'
-META_DESCRIPTION = u"""
-    Comunidade Python Brasil no Slack.
-    Slack é um app de comunicação com suporte a canais, conversas,
-    integração com seriços externos além de ser viciante."""
+META_TITLE = 'METATAG_TITLE'
+META_DESCRIPTION = 'METATAG_DESCRIPTION'
